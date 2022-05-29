a04eb5544b5487726d844a28bdd55454d90cae5b
Khairuddin Nasty <56571284+khairuddin-n@users.noreply.github.com>
2022-05-29 21:52:45 +0700

    Add files via upload

---
+++ F_5025201041.R
@@ -0,0 +1,43 @@
+# Soal 1
+# 1a
+# Data Sebelum melakukan aktivitas
+x <- c(78, 75, 67, 77, 70, 72, 28, 74, 77)
+# Data Setelah melakukan aktivitas
+y <- c(100, 95, 70, 90, 90, 90, 89, 90, 100)
+
+sd(x-y)
+
+# 1b
+t.test(x, y, alternative = "greater", var.equal = FALSE)
+
+# 1c
+var.test(x, y)
+t.test(x, y, mu = 0, alternative = "two.sided", var.equal = TRUE)
+
+# Soal 2
+zsum.test(mean.x=23500, sigma.x = 3900, n.x = 100,  
+          alternative = "less", mu = 20000,
+          conf.level = 0.95)
+
+# Soal 3
+# soal 3b 
+tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, mean.y =2.79 , s.y = 1.32, 
+          n.y = 27, alternative = "two.side", var.equal = TRUE)
+
+# soal 3c
+n1 <- 19
+n2 <- 27
+mean1 <- 3.64
+mean2 <- 2.79
+sd1 <- 1.67
+sd2 <- 1.32
+alpha <- 0.05
+df <- 2
+
+t_tabel <- qt(p=alpha/2,df=df,lower.tail = FALSE)
+t_tabel
+# soal 3d
+sp <- sqrt(((n1-1)*sd1^2 + (n2-1)*sd2^2)/(df))
+sp
+T_value <- (mean1-mean2)/(sp*sqrt((1/n1)+(1/n2)))
+T_value
