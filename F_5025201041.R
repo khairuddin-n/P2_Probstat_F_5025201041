4d25b92ffe54d3d9073f4c008388eb95498f7c40
Khairuddin Nasty <56571284+khairuddin-n@users.noreply.github.com>
2022-05-29 22:58:01 +0700

    Update F_5025201041.R
    4

--- F_5025201041.R
+++ F_5025201041.R
@@ -20,11 +20,11 @@ zsum.test(mean.x=23500, sigma.x = 3900, n.x = 100,
           conf.level = 0.95)
 
 # Soal 3
-# soal 3b 
+# 3b 
 tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, mean.y =2.79 , s.y = 1.32, 
           n.y = 27, alternative = "two.side", var.equal = TRUE)
 
-# soal 3c
+# 3c
 n1 <- 19
 n2 <- 27
 mean1 <- 3.64
@@ -36,8 +36,28 @@ df <- 2
 
 t_tabel <- qt(p=alpha/2,df=df,lower.tail = FALSE)
 t_tabel
-# soal 3d
+# 3d
 sp <- sqrt(((n1-1)*sd1^2 + (n2-1)*sd2^2)/(df))
 sp
 T_value <- (mean1-mean2)/(sp*sqrt((1/n1)+(1/n2)))
 T_value
+
+# soal 4
+# 4a
+df4 <- read.delim("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt")
+
+library(ggpubr)
+ggboxplot(df4, x = "Group", y = "Length", 
+          color = "Group",
+          ylab = "Length", xlab = "Group")
+
+# 4b
+library(onewaytests)
+bartlett.test(Length ~ Group, data = df4)
+
+# 4c
+Model1 <- lm(formula = Group ~ Length, data = df4)
+print(Model1)
+
+# 4d
+summary(Model1)$coefficients[2,4]
